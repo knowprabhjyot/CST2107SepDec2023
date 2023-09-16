b376939794ffc92c337852cb7a3e0927b508e08a
Prabhjyot Gambhir <knowprabhjyot@gmail.com>
2023-09-16 12:07:32 -0700

    Week 2

---
+++ Week 2/viteproject/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Footer from './footer'
+
+function App() {
+
+  return (
+  <div>
+    Hello world
+    <Footer />
+  </div>
+  )
+}
+
+export default App
---
+++ Week 2/viteproject/src/footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>
+            Copyright @Prabh
+        </footer>
+    )
+}
+
+export default Footer;
